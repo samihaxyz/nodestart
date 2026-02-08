51de92bce230eb70183e866cbd1242a071e7d6d0
samihaxyz <ug2202028@cse.pstu.ac.bd>
2026-02-08 15:49:35 +0600

    add tests

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import stylisticJs from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    },
+  },
+  { 
+    ignores: ['dist/**'], 
+  },
+]);
